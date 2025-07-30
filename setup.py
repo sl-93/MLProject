15d49f0cdef4ee1036b9b47fbb566712022fc6c2
sl-93 <slotfi72@gmail.com>
2025-07-30 10:45:55 +0330

    setup created

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements() -> List[str]:
+    """
+    This function will return list of requirements
+    """
+    requirement_lst:List[str] = []
+    try:
+        with open('requirements.txt', 'r') as file:
+            # Read lines from the file
+            lines = file.readlines()
+
+            # Process each line
+            for line in lines:
+                requirement = line.strip()
+
+                # Ignore empty lines and -e .
+                if requirement and requirement != '-e .':
+                    requirement_lst.append(requirement)
+
+    except FileNotFoundError:
+        print("requirements.txt file not found!")
+    
+    return requirement_lst
+
+
+setup(
+    name = 'mlproject',
+    version = '0.0.1',
+    author = 'Saeed',
+    author_email = 'slotfi72@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements()
+)
